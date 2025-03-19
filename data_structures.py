d9ecea2e58915afa2a81c3aa1f97e397496f28d4
cyberbot777 <cyberbot7777777@gmail.com>
2025-03-19 10:49:31 -0500

    Initial data structures excercise with multiple students and averages

--- data_structures.py
+++ data_structures.py
@@ -1,22 +1,30 @@
-student = {"name": "Richard", "age": 25, "grades": [85, 90, 95]}
+
+
+student1 = {"name": "Richard", "age": 25, "grades": [85, 90, 95]}
 student2 = {"name": "Alice", "age": 22, "grades": [90, 85, 92]}
 student3 = {"name": "Mike", "age": 29, "grades": [99, 86, 90]}
-student["grades"].append(88)
+
+student1["grades"].append(88)
 student2["grades"].append(84)
-average = sum(student["grades"]) / len(student["grades"])
-print(f"Student: {student}")
-print(f"Average grade: {average}")
 
-for grade in student["grades"]:
-    if grade >=90:
-        print(f"{grade} is an A!")
-    else:
-        print(f"{grade} is a B or lower.")
+student1["major"] = "Computer Science"
 
-student["major"] = "Computer Science"
-print(f"Student with major: {student}")
+average1 = sum(student1["grades"]) / len(student1["grades"])
+print(f"Richard's updated info: {student1}")
+print(f"Richard's average : {average1}")
 
-students = [student, student2, student3]
-for s in students:
-    avg = sum(s["grades"]) / len(s["grades"])
-    print(f"{s['name']}'s average: {avg}")
+students = [student1, student2, student3]
+
+print("\nCalculating averages for all students:")
+for student in students:
+    avg = sum(student["grades"]) / len(student["grades"])
+    print(f"{student['name']}'s : {avg}")
+
+print("\nClassifying grades:")
+for student in students:
+    print(f"\nGrades for {student['name']}")
+    for grade in student["grades"]:
+        if grade >= 90:
+            print(f"{grade} is an A!")
+        else:
+            print(f"{grade} is a B or lower.")
